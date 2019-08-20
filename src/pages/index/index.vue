41809936cef4d87f5c06fe454c50036172e4e0a9
apple <apple@appledeMacBook-Pro-3.local>
2019-08-20 20:06:53 +0800

    SearchBar-component

---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <SearchBar
+      :disabled="true"
+      @onClick="onSearchaBarClick"
+    >//  等价于直接写  disable
+    </SearchBar>
+  </div>
+</template>
+
+<script>
+  import SearchBar from '../../components/home/SearchBar'
+
+  export default {
+    components: {
+      SearchBar
+    },
+    methods: {
+      onSearchaBarClick() {
+        //  跳转到搜索页面
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
