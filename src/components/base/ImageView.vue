e6b5d983828afc357c4c080f1c1e519089955528
apple <apple@appledeMacBook-Pro-3.local>
2019-08-21 11:30:29 +0800

    ImageView

---
+++ src/components/base/ImageView.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="image-view">
+    <img
+      :class="round ? 'round image' : 'image'"
+      :style="{ height }"
+      :src="src"
+      :mode="mode"
+      :lazy-load="lazyLoad"
+      @load="onLoad"
+      @error="onError"
+      v-show="!isLoading && ! error"
+    />
+    <img
+      :class="round ? 'round image' : 'image'"
+      :style="{ height }"
+      src="https://www.youbaobao.xyz/book/img/loading2.ae9e5924.jpeg"
+      :mode="mode"
+      :lazy-load="lazyLoad"
+      v-show="isLoading || error"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ImageView',
+    props: {
+      src: {
+        type: String,
+        default: ''
+      },
+      mode: {
+        type: String,
+        default: 'widthFix'
+      },
+      lazyLoad: {
+        type: Boolean,
+        default: true
+      },
+      round: {
+        type: Boolean,
+        default: false
+      },
+      height: {
+        type: String,
+        default: 'auto'
+      }
+    },
+    watch: {
+      src(newValue, preValue) {
+
+      }
+    },
+    data() {
+      return {
+        isLoading: true,
+        error: false
+      }
+    },
+    methods: {
+      onClick() {
+        this.$emit('onClick')
+      },
+      onError() {
+        this.error = true
+        this.isLoading = false
+        console.log('onError')
+      },
+      onLoad() {
+        this.isLoading = false
+        this.error = false
+        console.log('onLoad')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.image-view {
+  width: 100%;
+  .image {
+    width: 100%;
+    &.round {
+      border-radius: 50%;
+     }
+  }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,15 +5,24 @@
       @onClick="onSearchaBarClick"
     >//  等价于直接写  disable
     </SearchBar>
+    <ImageView
+      src="https://www.youbaobao.xyz/mpvue-res/big.jpg"
+      round
+      mode="scaleToFill"
+      height="200px"
+    >      //高度自适应的写法  height="300px" mode="scaleToFill"
+    </ImageView>
   </div>
 </template>
 
 <script>
   import SearchBar from '../../components/home/SearchBar'
+  import ImageView from '../../components/base/ImageView'
 
   export default {
     components: {
-      SearchBar
+      SearchBar,
+      ImageView
     },
     methods: {
       onSearchaBarClick() {
